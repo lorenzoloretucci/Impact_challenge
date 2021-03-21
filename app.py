97b0b8ef48018fd4fba274380ca4e271eb7f1ef4
lorenzoloretucci <lorenzo.loretucci@gmail.com>
2021-03-21 18:39:21 +0100

    lots of codeeee

--- app.py
+++ app.py
@@ -3,14 +3,43 @@ import dash_core_components as dcc
 import dash_html_components as html
 import pandas as pd
 import numpy as np
+import folium 
 from dash.dependencies import Output, Input
+import schedule 
+import threading, time 
 
 print(dash.__version__)
 
-data = pd.read_csv('avocado.csv')
 
-data["Date"] = pd.to_datetime(data["Date"], format ="%Y-%m-%d" )
-data.sort_values("Date", inplace = True)
+
 
 app = dash.Dash(__name__)
 app.title = "UnWaste! Project"
+
+app.layout = html.Div(
+    children = [
+        html.Div(children = [html.H1("UnWaste!"),html.P('Prova della dashboard!')], className = 'Header'),
+        html.Div(children = [html.Iframe(id = 'map', srcDoc = open("ROme.html", "r").read(), width = "50%", height = "500", style={'display': 'inline-block'}),
+                             dcc.Graph(id="graph2",style={'display': 'inline-block', 'height' : "600"})])    
+    ]
+)
+
+
+#@app.callback(dash.dependencies.Output('map', 'srcDoc'))
+def update_map():
+    print("Dai che vaaaaa")
+#    return open('ROme.html', 'r').read()
+
+schedule.every(10).seconds.do(update_map)
+
+def my_thread_proc():
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
+t = threading.Thread(target = my_thread_proc, daemon = True)
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
+    t.run()
+
