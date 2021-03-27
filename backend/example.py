c844d35f79834be78cc06865803e3c553d27c63b
Sasha Melnic <56506760+AlexandrMelnic@users.noreply.github.com>
2021-03-27 12:53:51 +0100

    Update example.py

--- backend/example.py
+++ backend/example.py
@@ -3,9 +3,9 @@ import prediction, zone_splitting, path_planning
 
 DIR_PATH = '/content/Impact_challenge'
 
-predictor = MakePrediction(DIR_PATH)
+predictor = prediction.MakePrediction(DIR_PATH)
 pred = predictor.prediction()
 
 zones_split = zone_splitting.kmeans_subdivision(pred, DIR_PATH, 2)
 
-paths = path_planning(zones_split, DIR_PATH)
+paths = path_planning.path_planning(zones_split, DIR_PATH)
