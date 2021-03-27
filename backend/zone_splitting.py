2eabb636ec52af6414cc32af3c1ffdeb818446de
Sasha Melnic <56506760+AlexandrMelnic@users.noreply.github.com>
2021-03-27 11:28:38 +0100

    Update zone_splitting.py

--- backend/zone_splitting.py
+++ backend/zone_splitting.py
@@ -1,14 +1,17 @@
 from sklearn.cluster import KMeans
 import numpy as np
 import pandas as pd
+from collections import defaultdict
 
 def kmeans_subdivision(bin_ids, DIR_PATH, k):
+
     '''
     Function that takes the ids of the bins and splits them into 
     zones by performing a KMeans clustering. Takes in input the array of
     bin ids, accesses the DB and takes coordinates and performs the 
     clustering. Returns an array of (bin_idx, cluster_labels).
     '''
+
     data = pd.read_csv(DIR_PATH + '/DATABASE/coords_groups.csv')
     coords = data[['latitude','longitude']].values
     coords = coords[bin_ids]
@@ -17,4 +20,8 @@ def kmeans_subdivision(bin_ids, DIR_PATH, k):
     cl = KMeans(k)
     cl.fit(coords)
 
-    return np.stack((bin_ids, cl.labels_), axis=1)
+    out = defaultdict(list)
+    for key, val in zip(cl.labels_, bin_ids):
+        out[key].append(val)
+
+    return out
